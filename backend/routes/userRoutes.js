cb89c59f0aed8a4b577c03469f891adf1904683c
Giorgio Formiggini <giorgio.formiggini@edu-its.it>
2025-10-21 16:30:14 +0200

    Update userRoutes.js

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,8 +3,6 @@ const express = require('express');
 const router = express.Router();
 
 // Import middleware
-const verifyToken = require('./middleware/authMiddleware');
-const userController = require('./controller/userController');
 
 // GET /api/user/profile
 router.get('/profile', verifyToken, userController.getProfile);
